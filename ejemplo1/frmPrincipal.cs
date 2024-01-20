b2573e4f5cd8c37090c391fab3d213c8cd3cd050
ceballosAgustin <aceballos1923@gmail.com>
2024-01-20 17:23:27 -0300

    segunda subida...

--- ejemplo1/frmPrincipal.cs
+++ ejemplo1/frmPrincipal.cs
@@ -14,6 +14,7 @@ namespace ejemplo1
     {
         public frmPrincipal()
         {
+            // Agrego un comentario para la segunda subida en Git
             InitializeComponent();
         }
 
